b6d6d223511913143af5744e71b050f31348f251
Jeremy Fix <jeremy.fix@gmail.com>
2016-07-30 01:34:25 +0200

    upd

--- Lasagne/LogisticRegression/classify_mnist.py
+++ Lasagne/LogisticRegression/classify_mnist.py
@@ -1,3 +1,7 @@
+# TODO :
+# save the learning curves with a small description at the beginning of the setting
+
+
 import matplotlib.pyplot as plt
 
 import sys
@@ -39,51 +43,21 @@ learning_algo.add_argument('--sgd', type=str, help='Stochastic gradient descent'
 learning_algo.add_argument('--adagrad', type=str, help='Adaptive gradient')
 
 parser.add_argument('--momentum', type=float, help='Momentum value')
+parser.add_argument('--nesterov_momentum', type=float, help='Nesterov momentum value')
 parser.add_argument('--l2', type=float, help='L2 regularization')
 parser.add_argument('--l1', type=float, help='L1 regularization')
+
+parser.add_argument('--nbtrain', type=int, help='Number of training samples', default=60000)
+parser.add_argument('--nbtest', type=int, help='Number of test samples', default=10000)
+parser.add_argument('--batchsize', type=int, help='Minibatch size', default=600)
+parser.add_argument('--epoch', type=int, help='Number of training epochs', default=80)
 
 # Parse the arguments and perform some clean up dropping out the None values
 args = {k:v for k, v in vars(parser.parse_args()).iteritems() if v}
 print(args)
 
 ################################################################
-# Print which setup we consider
-################################################################
 
-print("Model architecture : ")
-if('logreg' in args):
-    print("    Logistic regression, Input -> Softmax")
-elif 'cnn' in args:
-    print("    Convolutional neural network with :")
-elif 'vgg' in args:
-    print("    Oxford Visual Geometry Group with :")
-print("")
-print("Learning algorithm :")
-if 'sgd' in args:
-    learning_rate = float(args['sgd'])
-    print("    Stochastic Gradient descent")
-    print("      * Learning rate : %f" % learning_rate)
-elif 'adagrad' in args:
-    print("    Adaptive Gradient descent")
-    print("      * Learning rate : %f" % 0)
-print("")
-print("Regularization :")
-if 'l2' in args:
-    print("    L2 regularization %f" % args['l2'])
-elif 'l1' in args:
-    print("    L1 regularization %f" % args['l1'])
-else:
-    print("    No regularieation")
-
-
-# Leads to 91.5 % test accuracy
-
-minibatch_size = 600
-nb_training_samples = 60000
-nb_test_samples = 10000
-max_epoch = 80
-momentum = 0.9
-weight_decay = 0.0001
 
 def iterate_minibatches(inputs, targets, batchsize):
     p = np.random.permutation(inputs.shape[0])
@@ -91,19 +65,27 @@ def iterate_minibatches(inputs, targets, batchsize):
         yield (inputs[p])[idx:idx+batchsize],(targets[p])[idx:idx+batchsize]
 
 print("Loading the data")
-X_train, y_train, X_test, y_test = mnist.load_dataset(nb_training_samples, nb_test_samples, False)
+X_train, y_train, X_test, y_test = mnist.load_dataset(args['nbtrain'], args['nbtest'], False)
 
 ############################################################################
 # Model building
 
-print("Building the model")
-
-if 'logreg' in args:
+print("Model architecture : ")
+if('logreg' in args):
+    print("    Logistic regression, Input -> Softmax")
     model = LogisticRegression.Model(28, 28, 1, 10)
+elif 'mpl' in args:
+    print("    Multilayer perceptron with : ")
+    sys.exit()
 elif 'cnn' in args:
+    print("    Convolutional neural network with :")
     sys.exit()
 elif 'vgg' in args:
+    print("    Oxford Visual Geometry Group with :")
     sys.exit()
+else:
+    sys.exit()
+print("")
 
 input_var, l_out = model.get_input_var(), model.get_output_layer()
 prediction = lasagne.layers.get_output(l_out)
@@ -113,13 +95,54 @@ target_var = T.ivector('targets')
 loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
 loss = loss.mean()
 
-l2 = lasagne.regularization.regularize_network_params(l_out, lasagne.regularization.l2)
-loss += weight_decay * l2
+print("Regularization :")
+if 'l2' in args:
+    print("    L2 regularization of lambda = %f" % args['l2'])
+    l2 = lasagne.regularization.regularize_network_params(l_out, lasagne.regularization.l2)
+    loss += args['l2'] * l2
+elif 'l1' in args:
+    print("    L1 regularization of lambda = %f " % args['l1'])
+    l1 = lasagne.regularization.regularize_network_params(l_out, lasagne.regularization.l1)
+    loss += args['l1'] * l1
 
 # Define the update of the weights
 params = lasagne.layers.get_all_params(l_out, trainable=True)
-updates = lasagne.updates.adagrad(loss, params)
-updates = lasagne.updates.apply_momentum(updates, momentum=momentum)
+
+print("Learning algorithm :")
+if 'sgd' in args:
+    learning_rate = float(args['sgd'])
+    print("    Stochastic Gradient descent")
+    print("      * Learning rate : %f" % learning_rate)
+    updates = lasagne.updates.sgd(loss, params, learning_rate)
+elif 'adagrad' in args:
+    learning_rate = float(args['adagrad'])
+    print("    Adaptive Gradient descent")
+    print("      * Learning rate : %f" % learning_rate)
+    momentum = 0.9
+    if(momentum in args):
+        momentum = args['momentum']
+    print("      * Momentum : %f " % momentum)
+    updates = lasagne.updates.adagrad(loss, params, learning_rate, 0.9)
+print("")
+
+
+print("Regularization :")
+if 'l2' in args:
+    print("    L2 regularization %f" % args['l2'])
+elif 'l1' in args:
+    print("    L1 regularization %f" % args['l1'])
+else:
+    print("    No regularization")
+
+# Momentum ?
+if 'momentum' in args and not 'adagrad' in args:
+    print("Applying a momentum of %f " % args['momentum'])
+    updates = lasagne.updates.apply_momentum(updates, momentum=args['momentum'])
+elif 'nesterov_momentum' in args and not 'adagrad' in args:
+    print("Applying a Nesterov momentum of %f " % args['nesterov_momentum'])
+    updates = lasagne.updates.apply_nesterov_momentum(updates, momentum=args['nesterov_momentum'])
+
+print("\n  ==>  I have %i parametes to update  <== \n" % lasagne.layers.count_params(l_out))
 
 # Define the function to compute the test loss and test accuracy
 accuracy = lasagne.objectives.categorical_accuracy(prediction, target_var)
@@ -142,11 +165,11 @@ test_accu_arr.append(accu_fn(X_test, y_test))
 train_loss_arr.append(loss_fn(X_test, y_test))
 test_loss_arr.append(loss_fn(X_test, y_test))
 
-for i in range(max_epoch):
-    for Xi, yi in iterate_minibatches(X_train, y_train, minibatch_size):
+for i in range(args['epoch']):
+    for Xi, yi in iterate_minibatches(X_train, y_train, args['batchsize']):
         sys.stdout.write('\rEpoch : %f ' % epoch)
         train_fn(Xi, yi)
-        epoch += 1. / (nb_training_samples/float(minibatch_size))
+        epoch += 1. / (args['nbtrain']/float(args['batchsize']))
     train_accu_arr.append(accu_fn(X_train, y_train))
     test_accu_arr.append(accu_fn(X_test, y_test))
     train_loss_arr.append(loss_fn(X_train, y_train))
@@ -158,13 +181,13 @@ print(" Test accuracy : %f\n" % test_accu_arr[-1])
 
 plt.figure()
 plt.subplot(1,2,1)
-plt.plot(np.arange(max_epoch+1), train_accu_arr, 'b-', label='Train accuracy')
-plt.plot(np.arange(max_epoch+1), test_accu_arr, 'r-', label='Test accuracy')
+plt.plot(np.arange(args['epoch']+1), train_accu_arr, 'b-', label='Train accuracy')
+plt.plot(np.arange(args['epoch']+1), test_accu_arr, 'r-', label='Test accuracy')
 plt.legend()
 
 plt.subplot(1,2,2)
-plt.plot(np.arange(max_epoch+1), train_loss_arr, 'b-', label='Train loss')
-plt.plot(np.arange(max_epoch+1), test_loss_arr, 'r-', label='Test loss')
+plt.plot(np.arange(args['epoch']+1), train_loss_arr, 'b-', label='Train loss')
+plt.plot(np.arange(args['epoch']+1), test_loss_arr, 'r-', label='Test loss')
 plt.legend()
 
 plt.show()
