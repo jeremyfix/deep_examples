1d8f96bab0c33f688ad7ebe10101ade894e5ff5a
Jeremy Fix <Jeremy.Fix@centralesupelec.Fr>
2017-03-03 10:12:09 +0100

    upd

--- Keras/Example1/mnist.py
+++ Keras/Example1/mnist.py
@@ -5,7 +5,7 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Input
 from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
+from keras.layers.pooling import MaxPooling2D, GlobalAveragePooling2D
 from keras.layers.normalization import BatchNormalization
 from keras import backend as K
 from keras.utils import np_utils
@@ -63,10 +63,9 @@ def build_network(input_shape):
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid'))
     
+    model.add(GlobalAveragePooling2D())
     
-    model.add(Flatten())
-    
-    model.add(Dense(32))
+    model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dense(10))
     model.add(Activation('softmax'))
