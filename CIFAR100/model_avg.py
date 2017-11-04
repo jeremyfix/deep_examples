fb7c8587588ba5078ff4b8d1e8a9eb3ae0ea813a
Jeremy Fix <jeremy.fix@gmail.com>
2017-11-04 19:06:46 +0100

    upd

--- CIFAR100/model_avg.py
+++ CIFAR100/model_avg.py
@@ -15,6 +15,7 @@ num_classes = 100
 y_test_cat = to_categorical(y_test, num_classes=num_classes)
 
 def acc_loss(logits, y_classes):
+    print(logits, y_classes)
     loss = -np.log(logits[:,y_classes]).sum()/y_classes.shape[0]
     pred_classes = np.argmax(logits, axis=1)
     acc = (pred_classes == y_classes).sum()/y_classes.shape[0]
@@ -29,11 +30,11 @@ for f in sys.argv[1:]:
     print("Computing the predictions of {}".format(f))
 
     pred = model.predict(x_test,verbose=0)
-    predictions.append(pred)
-    scores = model.evaluate(x_test, y_test_cat, verbose=0)
+    scores = model.evaluate(x_test[:2,:,:,:], y_test_cat[:2], verbose=0)
     print("From eval : {}".format(scores))
-    scores = acc_loss(pred, y_test)
+    scores = acc_loss(pred[:2], y_test[:2])
     print(scores)
+    predictions.append(pred)
     
 predictions = np.array(predictions)
 print(predictions.shape)
