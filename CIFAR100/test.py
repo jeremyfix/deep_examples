c12c94195fe4a3843b839364d83b8098ca8ed271
Jeremy Fix <jeremy.fix@gmail.com>
2017-11-04 18:47:27 +0100

    upd

--- CIFAR100/test.py
+++ CIFAR100/test.py
@@ -1,5 +1,6 @@
 from keras.models import load_model
 from keras.datasets import cifar100
+from keras.utils import to_categorical
 import sys
 
 if len(sys.argv) != 2:
@@ -8,6 +9,9 @@ if len(sys.argv) != 2:
 
 print("Loading the dataset")
 (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+
+num_classes = 100
+y_test = to_categorical(y_test, num_classes=num_classes)
 
 print("Loading the model")
 model = load_model(sys.argv[1])
