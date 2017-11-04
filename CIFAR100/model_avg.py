2c00cd0b1da5f78a48fd7f5ea33c6f5806710319
Jeremy Fix <jeremy.fix@gmail.com>
2017-11-04 20:52:01 +0100

    upd

--- CIFAR100/model_avg.py
+++ CIFAR100/model_avg.py
@@ -25,9 +25,9 @@ def acc_loss(logits, y_classes):
 print("Loading the models and computing their predictions")
 predictions = []
 for idx, f in enumerate(sys.argv[1:]):
-    with h5py.File(f, 'a') as f:
-        if 'optimizer_weights' in f.keys():
-            del f['optimizer_weights']
+    with h5py.File(f, 'a') as fh5:
+        if 'optimizer_weights' in fh5.keys():
+            del fh5['optimizer_weights']
     
     model = load_model(f)
 
