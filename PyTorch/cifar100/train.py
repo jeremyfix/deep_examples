cb28368ee84567bf1b793073c2491629ee9842f1
Jeremy Fix <jeremy.fix@gmail.com>
2017-11-21 14:43:05 +0100

    upd

--- PyTorch/cifar100/train.py
+++ PyTorch/cifar100/train.py
@@ -9,6 +9,11 @@ from torch.autograd import Variable
 import torchvision
 from torchvision import datasets, transforms
 
+# pip3 install nibabel tqdm
+# pip3 install git+https://github.com/ncullen93/torchsample
+import torchsample
+from torchsample.transforms import RandomAffine
+
 from utils import progress_bar, torch_summarize, split
 
 import matplotlib
@@ -21,9 +26,9 @@ import numpy as np
 
 batch_size = 32
 
-#dataset_path = "/home/fix_jer/Datasets"
+dataset_path = "/home/fix_jer/Datasets"
 #dataset_path = "/usr/users/ims/fix_jer/Datasets"
-dataset_path = "/opt/Datasets/"
+#dataset_path = "/opt/Datasets/"
 
 classnames = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
 
@@ -43,6 +48,7 @@ data_transforms = {
         transforms.RandomHorizontalFlip(),
         transforms.RandomCrop(32, padding=5),
         transforms.ToTensor(),
+        RandomAffine(zoom_range=(0.8, 1.2)),
         transforms.Normalize(mean, std)
     ]),
     'val': transforms.Compose([
