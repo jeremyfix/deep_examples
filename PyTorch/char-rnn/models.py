b49c6c39d944f6eade73c05b705255bb0052e0a4
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2020-12-21 14:44:10 +0100

    we need to provide the device for when running on gpu

--- PyTorch/char-rnn/models.py
+++ PyTorch/char-rnn/models.py
@@ -28,9 +28,10 @@ class Model(nn.Module):
             batch, seq_len = 1, x.shape[0]
         else:
             batch, seq_len = x.shape
+        device = x.device
         vocab_size = self.vocab_size
         x_idx = x.view(-1, 1)
-        inputs = torch.zeros(seq_len*batch, vocab_size)
+        inputs = torch.zeros(seq_len*batch, vocab_size, device=device)
         inputs.scatter_(1, x_idx, 1)
         inputs = torch.transpose(inputs.view(batch, seq_len, vocab_size), 0, 1)
         return inputs
@@ -39,13 +40,14 @@ class Model(nn.Module):
         # x is (batch, seq_len)
         (batch, seq_len), vocab_size = x.shape, self.vocab_size
 
+        device = x.device
         # the inputs to the LSTM must be (seq_len, batch, input_size)
         # we first need to one-hot encode the inputs
         inputs = self.to_one_hot(x)
 
         # input to the LSTM must be (seq_len, batch, vocab_size)
         # h0, c0 are (num_layers, batch, hidden_size)
-        h0 = c0 = torch.zeros(self.num_layers, batch, self.num_cells)
+        h0 = c0 = torch.zeros(self.num_layers, batch, self.num_cells, device=device)
         output, (hn, cn) = self.rnn(inputs, (h0, c0))
 
         # output is (seq_len, batch, num_cells)
@@ -62,14 +64,14 @@ class Model(nn.Module):
             print("""We expect just a one dimensional array for the input when
                   sampling""")
         batch = 1
-        inputs = self.to_one_hot(x)
         # inputs is (seq_len, batch, vocab_size)
+        device = x.device
         generated_seq = []
-        h0 = c0 = torch.zeros(self.num_layers, batch, self.num_cells)
+        h0 = c0 = torch.zeros(self.num_layers, batch, self.num_cells, device=device)
         next_char_idx = None
         # Feed the network with the starting tensor
         for xtidx in x:
-            xt = torch.zeros(1, 1, self.vocab_size)
+            xt = torch.zeros(1, 1, self.vocab_size, device=device)
             xt[0, 0, xtidx] = 1
             output, (h0, c0) = self.rnn(xt, (h0, c0))
             output = output.transpose(0, 1)
@@ -78,7 +80,7 @@ class Model(nn.Module):
 
         generated_seq.append(next_char_idx)
         for it in range(length):
-            xt = torch.zeros(1, 1, self.vocab_size)
+            xt = torch.zeros(1, 1, self.vocab_size, device=device)
             xt[0, 0, next_char_idx] = 1
             output, (h0, c0) = self.rnn(xt, (h0, c0))
             output = output.transpose(0, 1)
