a972218d41424668764d27027990076fa3de47a3
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-12-08 19:38:54 +0100

    make the shell script a sbatch script

--- PyTorch/cifar100/run_expe.sh
+++ PyTorch/cifar100/run_expe.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-python3 train.py --batch_size 32 --base_lrate 0.01 
-python3 train.py --batch_size 32 --base_lrate 0.01 --bn
-python3 train.py --batch_size 32 --base_lrate 0.01 --bn --data_augment 
-python3 train.py --batch_size 32 --base_lrate 0.01 --bn --data_augment --l2_reg --dropout --lsmooth
+#SBATCH --job-name=cifar100
+#SBATCH --nodes=1
+#SBATCH --partition=gpu_prod_long
+#SBATCH --time=48:00:00
+
+python3 -m venv $TMPDIR/venv
+source $TMPDIR/venv/bin/activate
+python -m pip install torch torchvision deepcs matplotlib numpy
+
+python train.py --batch_size 32 --base_lrate 0.01 
+python train.py --batch_size 32 --base_lrate 0.01 --bn
+python train.py --batch_size 32 --base_lrate 0.01 --bn --data_augment 
+python train.py --batch_size 32 --base_lrate 0.01 --bn --data_augment --l2_reg --dropout --lsmooth
